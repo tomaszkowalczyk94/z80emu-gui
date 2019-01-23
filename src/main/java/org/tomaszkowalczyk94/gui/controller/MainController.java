a5e950ea7f0e6665027a2a0110d4fc67937ccc7d
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2019-01-23 20:25:28 +0100

    menu of items

--- src/main/java/org/tomaszkowalczyk94/gui/controller/MainController.java
+++ src/main/java/org/tomaszkowalczyk94/gui/controller/MainController.java
@@ -1,9 +1,15 @@
 package org.tomaszkowalczyk94.gui.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import org.tomaszkowalczyk94.gui.model.Context;
 
 public class MainController {
+
+
 
     private Context context = new Context();
 
@@ -11,6 +17,9 @@ public class MainController {
     @FXML private MemoryController memoryController;
     @FXML private RegistersController registersController;
     @FXML private AssemblerController assemblerController;
+
+    @FXML public BorderPane mainBorderPane;
+    @FXML public MenuItem closeMenuItem;
 
     public void initialize() {
 
@@ -26,4 +35,27 @@ public class MainController {
         debuggerController.setRegistersController(registersController);
 
     }
+
+
+    public void onOpenAsmFileClicked(ActionEvent actionEvent) {
+    }
+
+    public void onSaveAsmToFileClicked(ActionEvent actionEvent) {
+    }
+
+    public void onClearMemoryClicked(ActionEvent actionEvent) {
+    }
+
+    public void onLoadMemoryFromFileClicked(ActionEvent actionEvent) {
+    }
+
+    public void onSaveMemoryToFileClicked(ActionEvent actionEvent) {
+    }
+
+    public void onCloseClicked() {
+        Stage stage = (Stage)mainBorderPane.getScene().getWindow();
+        stage.close();
+    }
+
+
 }
